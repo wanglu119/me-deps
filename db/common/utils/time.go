f0a565bc4e908e7ef65f1b5de6ea4739f703047a
wanglu119 <805784956.qq.com>
2024-04-19 15:03:12 +0800

    fix: Time empty json decode error

--- db/common/utils/time.go
+++ db/common/utils/time.go
@@ -1,33 +1,37 @@
 package utils
 
 import (
-    "time"
+	"time"
 )
 
 type Time time.Time
 
 const (
-    timeFormart = "2006-01-02 15:04:05"
+	timeFormart = "2006-01-02 15:04:05"
 )
 
 func (t *Time) UnmarshalJSON(data []byte) (err error) {
-    now, err := time.ParseInLocation(`"`+timeFormart+`"`, string(data), time.Local)
-    *t = Time(now)
-    return
+	if len(data) > 2 {
+		now, err := time.ParseInLocation(`"`+timeFormart+`"`, string(data), time.Local)
+		*t = Time(now)
+		return err
+	}
+
+	return
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
-    b := make([]byte, 0, len(timeFormart)+2)
-    b = append(b, '"')
-    b = time.Time(t).AppendFormat(b, timeFormart)
-    b = append(b, '"')
-    return b, nil
+	b := make([]byte, 0, len(timeFormart)+2)
+	b = append(b, '"')
+	b = time.Time(t).AppendFormat(b, timeFormart)
+	b = append(b, '"')
+	return b, nil
 }
 
 func (t Time) String() string {
-    return time.Time(t).Format(timeFormart)
+	return time.Time(t).Format(timeFormart)
 }
 
 func (t Time) Format(format string) string {
-    return time.Time(t).Format(format)
+	return time.Time(t).Format(format)
 }
