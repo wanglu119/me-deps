5d5a450965e23d033e90fc70627d79935b358822
wanglu119 <805784956.qq.com>
2024-05-22 18:57:28 +0800

    feat: adapt mongo-driver 1.15.0

--- db/mgo/util.go
+++ db/mgo/util.go
@@ -1,19 +1,18 @@
 package mgo
 
 import (
-    "time"
-    "strings"
-    "fmt"
-    "reflect"
-    "regexp"
-    
-    "go.mongodb.org/mongo-driver/x/bsonx"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-    
-    "github.com/wanglu119/me-deps/db/common/config"
-)
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/wanglu119/me-deps/db/common/config"
+)
 
 func toTableName(clsName string) string {
 	reg := regexp.MustCompile("[A-Z]*[a-z0-9]+")
@@ -21,36 +20,42 @@ func toTableName(clsName string) string {
 	sarr := reg.FindAllString(string(src), -1)
 	tableName := strings.Join(sarr, "_")
 	tableName = strings.ToLower(tableName)
-	
+
 	return tableName
 }
 
 func GetCollection(t reflect.Type) *mongo.Collection {
 	tableName := toTableName(t.Name())
 	coll := client.Database(config.Database.Name).Collection(tableName)
-	
-	return coll	
+
+	return coll
 }
 
 func CreateUniqueIndex(t reflect.Type, keys ...string) {
 	tableName := toTableName(t.Name())
 	col := client.Database(config.Database.Name).Collection(tableName)
-	indexOpts := options.CreateIndexes().SetMaxTime(10*time.Second)
-	
+	indexOpts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+
 	indexView := col.Indexes()
-	keysDoc := bsonx.Doc{}
-	
+	keysDoc := bson.D{}
+
 	for _, key := range keys {
 		if strings.HasPrefix(key, "-") {
-			keysDoc = keysDoc.Append(strings.TrimLeft(key, "-"), bsonx.Int32(-1))	
+			// mongo-driver v1.15.0
+			keysDoc = append(keysDoc, bson.E{Key: strings.TrimLeft(key, "-"), Value: -1})
+			// mongo-driver v1.11.3
+			// keysDoc = keysDoc.Append(strings.TrimLeft(key, "-"), bsonx.Int32(-1))
 		} else {
-			keysDoc = keysDoc.Append(key, bsonx.Int32(1))
+			// mongo-driver v1.15.0
+			keysDoc = append(keysDoc, bson.E{Key: key, Value: 1})
+			// mongo-driver v1.11.3
+			// keysDoc = keysDoc.Append(key, bsonx.Int32(1))
 		}
 	}
-	
-	result, err := indexView.CreateOne(ctx, 
-		mongo.IndexModel {
-			Keys: keysDoc,
+
+	result, err := indexView.CreateOne(ctx,
+		mongo.IndexModel{
+			Keys:    keysDoc,
 			Options: options.Index().SetUnique(true),
 		},
 		indexOpts,
@@ -63,22 +68,31 @@ func CreateUniqueIndex(t reflect.Type, keys ...string) {
 func CreateIndex(t reflect.Type, keys ...string) {
 	tableName := toTableName(t.Name())
 	col := client.Database(config.Database.Name).Collection(tableName)
-	indexOpts := options.CreateIndexes().SetMaxTime(10*time.Second)
-	
+	indexOpts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+
 	indexView := col.Indexes()
-	keysDoc := bsonx.Doc{}
-	
+	// mongo-driver v1.11.3
+	// keysDoc := bsonx.Doc{}
+	// mongo-driver v1.15.0
+	keysDoc := bson.D{}
+
 	for _, key := range keys {
 		if strings.HasPrefix(key, "-") {
-			keysDoc = keysDoc.Append(strings.TrimLeft(key, "-"), bsonx.Int32(-1))	
+			// mongo-driver v1.15.0
+			keysDoc = append(keysDoc, bson.E{Key: strings.TrimLeft(key, "-"), Value: -1})
+			// mongo-driver v1.11.3
+			// keysDoc = keysDoc.Append(strings.TrimLeft(key, "-"), bsonx.Int32(-1))
 		} else {
-			keysDoc = keysDoc.Append(key, bsonx.Int32(1))
+			// mongo-driver v1.15.0
+			keysDoc = append(keysDoc, bson.E{Key: key, Value: 1})
+			// mongo-driver v1.11.3
+			// keysDoc = keysDoc.Append(key, bsonx.Int32(1))
 		}
 	}
-	
-	result, err := indexView.CreateOne(ctx, 
-		mongo.IndexModel {
-			Keys: keysDoc,
+
+	result, err := indexView.CreateOne(ctx,
+		mongo.IndexModel{
+			Keys:    keysDoc,
 			Options: options.Index(),
 		},
 		indexOpts,
@@ -87,5 +101,3 @@ func CreateIndex(t reflect.Type, keys ...string) {
 		log.Error(fmt.Sprintf("EnsureIndex error: %v", err))
 	}
 }
-
-
