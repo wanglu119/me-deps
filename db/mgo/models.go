525106602e9eab400b2d6248937da3251358d757
wanglu119 <805784956.qq.com>
2024-05-23 17:39:15 +0800

    mongo support auth

--- db/mgo/models.go
+++ db/mgo/models.go
@@ -1,12 +1,12 @@
 package mgo
 
 import (
-	"sync"
 	"context"
-	
+	"sync"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	
+
 	"github.com/wanglu119/me-deps/db/common/config"
 )
 
@@ -15,10 +15,10 @@ type MTable interface {
 }
 
 var (
-	once sync.Once
-	client *mongo.Client
-	ctx context.Context
-	cancelFunc context.CancelFunc
+	once         sync.Once
+	client       *mongo.Client
+	ctx          context.Context
+	cancelFunc   context.CancelFunc
 	legacyTables []interface{}
 )
 
@@ -34,9 +34,17 @@ func getEngine() (*mongo.Client, error) {
 		ctx, cancelFunc = context.WithCancel(context.Background())
 		clientOpt := options.Client()
 		clientOpt.Hosts = []string{config.Database.Host}
-		client ,err = mongo.Connect(ctx, clientOpt)
+		if len(config.Database.User) > 0 {
+			clientOpt.Auth = &options.Credential{}
+			clientOpt.Auth.Username = config.Database.User
+		}
+		if len(config.Database.Password) > 0 {
+			clientOpt.Auth.Password = config.Database.Password
+		}
+
+		client, err = mongo.Connect(ctx, clientOpt)
 	})
-	
+
 	return client, err
 }
 
@@ -44,19 +52,14 @@ func NewEngine() (err error) {
 	if _, err = getEngine(); err != nil {
 		return err
 	}
-	
+
 	for _, m := range legacyTables {
 		m.(MTable).CreateIndex()
 	}
-	
+
 	return nil
 }
 
 func GetEngine() *mongo.Client {
 	return client
 }
-
-
-
-
-
