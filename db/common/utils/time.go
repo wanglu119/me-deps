dfaa7a16928202271c7e79f63382c3bd79eae1be
wanglu119 <805784956.qq.com>
2024-07-25 18:55:35 +0800

    feat: time format support 2006-01-02

--- db/common/utils/time.go
+++ db/common/utils/time.go
@@ -1,35 +1,46 @@
 package utils
 
 import (
+	"fmt"
 	"time"
 )
 
 type Time time.Time
 
 const (
-	timeFormart = "2006-01-02 15:04:05"
+	timeFormartWithTime = "2006-01-02 15:04:05"
+	timeFormartNoTime   = "2006-01-02"
 )
 
 func (t *Time) UnmarshalJSON(data []byte) (err error) {
-	if len(data) > 2 {
-		now, err := time.ParseInLocation(`"`+timeFormart+`"`, string(data), time.Local)
+	// +2 为双引号
+	if len(data) == 19+2 {
+		now, err := time.ParseInLocation(`"`+timeFormartWithTime+`"`, string(data), time.Local)
 		*t = Time(now)
 		return err
 	}
 
-	return
+	if len(data) == 10+2 {
+		now, err := time.ParseInLocation(`"`+timeFormartNoTime+`"`, string(data), time.Local)
+		*t = Time(now)
+		return err
+	}
+
+	err = fmt.Errorf("unsupport date format: %s, length: %d", string(data), len(data))
+
+	return err
 }
 
 func (t Time) MarshalJSON() ([]byte, error) {
-	b := make([]byte, 0, len(timeFormart)+2)
+	b := make([]byte, 0, len(timeFormartWithTime)+2)
 	b = append(b, '"')
-	b = time.Time(t).AppendFormat(b, timeFormart)
+	b = time.Time(t).AppendFormat(b, timeFormartWithTime)
 	b = append(b, '"')
 	return b, nil
 }
 
 func (t Time) String() string {
-	return time.Time(t).Format(timeFormart)
+	return time.Time(t).Format(timeFormartWithTime)
 }
 
 func (t Time) Format(format string) string {
